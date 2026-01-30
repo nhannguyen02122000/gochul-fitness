1e432784632e30ad6b83bb2adbcc65b20eb1c5eb
nhan.nt <nhannguyenthanh@192.168.1.60>
2026-01-31 00:36:35 +0700

    chore: add clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import AntConfigProvider from '@/providers/AntConfigProvider'
+import { viVN } from '@clerk/localizations'
+import {
+  ClerkProvider
+} from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Noto_Sans } from 'next/font/google'
-import AntConfigProvider from '@/providers/AntConfigProvider'
 import './globals.css'
 
 const notoFont = Noto_Sans({
@@ -18,10 +22,20 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${notoFont.className} antialiased`}>
-        <AntConfigProvider>{children}</AntConfigProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      localization={viVN}
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true
+        }
+      }}
+      signInUrl={'/sign-in'}
+    >
+      <html lang='en'>
+        <body className={`${notoFont.className} antialiased`}>
+          <AntConfigProvider>{children}</AntConfigProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,31 @@
+import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
+
+export default function Page() {
+  return (
+    <div className='w-screen h-screen relative'>
+      <div className='w-full h-full absolute bg-red-300'>
+        <Image
+          src={'/images/login_background.jpg'}
+          fill
+          alt='background'
+          objectFit={'cover'}
+          objectPosition={'right bottom'}
+          className='blur-md'
+        />
+      </div>
+      <div className='w-full h-full flex items-center justify-center flex-grow'>
+        <SignIn
+          appearance={{
+            elements: {
+              formButtonPrimary:
+                'bg-red-500 hover:bg-red-400 !border-none !shadow-none'
+            }
+          }}
+          fallback={'/'}
+          fallbackRedirectUrl={'/'}
+        />
+      </div>
+    </div>
+  )
+}
